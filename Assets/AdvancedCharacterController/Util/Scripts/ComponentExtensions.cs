462f843a1a0ff1d4c9484057bc214efddcecaf5b
DoctorWolfy121 <williamswhitehouse@gmail.com>
2019-08-05 17:24:25 +0100

    Imported Util scripts

---
+++ Assets/AdvancedCharacterController/Util/Scripts/ComponentExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ComponentExtensions
+{
+    /// <summary>
+    /// Adds a component to this components gameobject
+    /// </summary>
+    /// <param name="component"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T AddComponent<T>(this Component component) where T : Component
+    {
+        return component.gameObject.AddComponent<T>();
+    }
+
+    /// <summary>
+    /// Gets a component and sends an error if one isn't attached
+    /// </summary>
+    /// <param name="component"></param>
+    /// <typeparam name="T">Component Type</typeparam>
+    /// <returns>Component</returns>
+    /// <exception cref="Exception"></exception>
+    public static T GetComponentRequired<T>(this Component component) where T : Component
+    {
+        T comp = component.gameObject.GetComponent<T>();
+        if (comp == null)
+        {
+            throw new Exception(typeof(T) + " is not attached to " + component.gameObject.name);
+        }
+
+        return comp;
+    }
+
+    /// <summary>
+    /// Gets a component or adds one if it isn't attached
+    /// </summary>
+    /// <param name="component"></param>
+    /// <typeparam name="T">Component Type</typeparam>
+    /// <returns>Component</returns>
+    public static T GetComponentOrAdd<T>(this Component component) where T : Component
+    {
+        T comp = component.gameObject.GetComponent<T>();
+        if (comp == null)
+        {
+            comp = component.gameObject.AddComponent<T>();
+        }
+
+        return comp;
+    }
+}
---
+++ Assets/AdvancedCharacterController/Util/Scripts/InputSelectorAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+// https://answers.unity.com/questions/1378822/list-of-tags-in-the-inspector.html
+
+[AttributeUsage(AttributeTargets.Field)]
+public class InputSelectorAttribute : PropertyAttribute
+{
+}
