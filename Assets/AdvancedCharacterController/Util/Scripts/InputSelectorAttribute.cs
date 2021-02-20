835997a10c13985deb125c8dc9397ad146d4ef5c
WSWhitehouse <williamswhitehouse@gmail.com>
2021-02-20 19:35:59 +0000

    Removed Util folder & Icon
    Util folder contains unrelated code to project. Also contains unused scripts that are not required. Removed Icon as it was very low resolution

--- Assets/AdvancedCharacterController/Util/Scripts/InputSelectorAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using UnityEngine;
-
-// https://answers.unity.com/questions/1378822/list-of-tags-in-the-inspector.html
-
-[AttributeUsage(AttributeTargets.Field)]
-public class InputSelectorAttribute : PropertyAttribute
-{
-}
--- Assets/AdvancedCharacterController/Util/Scripts/TagSelectorAttribute.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class TagSelectorAttribute : PropertyAttribute
-{
-    public bool UseDefaultTagFieldDrawer = false;
-}
